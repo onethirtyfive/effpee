858cbe5cc3360870776292545e296688ddfb3571
Joshua Morris <joshua@dailykos.com>
2019-02-24 00:29:11 -0800

    money

--- src/Effpee/USMoney.hs
+++ src/Effpee/USMoney.hs
@@ -20,7 +20,9 @@ evalCoin :: USCoin -> Int
 evalCoin Penny         = 1
 evalCoin Nickel        = 5
 evalCoin Dime          = 10
+evalCoin Quarter       = 25
 evalCoin OneDollarCoin = 100
+evalCoin TwoDollarCoin = 200
 
 evalCoins
   :: Many USCoin
@@ -29,13 +31,20 @@ evalCoins Empty     = 0
 evalCoins (x :. xs) = evalCoin x + evalCoins xs
 
 evalBill :: USBill -> Int
-evalBill = todo "Effpee.USMoney.evalBill"
+evalBill OneDollar        = 1
+evalBill TwoDollar        = 2
+evalBill FiveDollar       = 5
+evalBill TenDollar        = 10
+evalBill TwentyDollar     = 20
+evalBill FiftyDollar      = 50
+evalBill OneHundredDollar = 100
 
 -- Use @evalBill@ in this definition
 evalBills
   :: Many USBill
   -> Int
-evalBills = todo "Effpee.USMoney.evalBills"
+evalBills Empty   = 0
+evalBills (x:.xs) = evalBill x + (evalBills xs)
 
 -- Given a US bill/note produce the presient whose portrait appears on it.
 -- * $1   => Washington
@@ -48,10 +57,21 @@ evalBills = todo "Effpee.USMoney.evalBills"
 getPortrait
   :: USBill
   -> Portrait
-getPortrait FiveDollar = Lincoln -- To get you started with a passing case
-getPortrait _          = todo "Effpee.USMoney.getPortrait"
+getPortrait OneDollar        = Washington
+getPortrait TwoDollar        = Jefferson
+getPortrait FiveDollar       = Lincoln
+getPortrait TenDollar        = Hamilton
+getPortrait TwentyDollar     = Jackson
+getPortrait FiftyDollar      = Grant
+getPortrait OneHundredDollar = Franklin
 
 isPresident
   :: Portrait
   -> Boolean     -- ^ this is the @Boolean@ from the ADT module NOT the builtin @Bool@ type
-isPresident = todo "Effpee.USMoney.isPresident"
+isPresident Washington = Yeah
+isPresident Jefferson  = Yeah
+isPresident Lincoln    = Yeah
+isPresident Hamilton   = Nah
+isPresident Jackson    = Yeah
+isPresident Grant      = Yeah
+isPresident Franklin   = Nah
